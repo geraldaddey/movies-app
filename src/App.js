0a11f84bfdb11c2e5e07c0263b44670c05f8cbe1
geraldaddey <geraldaddey@yahoo.com>
2022-06-28 15:08:43 +0000

    search functionality added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React from "react";
 import { useEffect, useState } from "react";
+import { FaSearch } from "react-icons/fa";
 
 import Movie from "./Components/Movie";
 
@@ -8,10 +9,11 @@ const FEATURED_API =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=db80d1db4faebe3092bdd4cfb9f63fe9";
 
 const SEARCH_API =
-  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+  "https://api.themoviedb.org/3/search/movie?&api_key=db80d1db4faebe3092bdd4cfb9f63fe9&query=";
 
 function App() {
   const [movies, SetMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   // fetch movies data from API and set it to new state
   useEffect(() => {
@@ -23,11 +25,48 @@ function App() {
       });
   }, []);
 
+  // const handleClick = () => {
+  //   console.log("button clicked");
+
+  //   // fetch(SEARCH_API + searchTerm)
+  //   //   .then((res) => res.json())
+  //   //   .then((data) => {
+  //   //     console.log(data);
+  //   //     SetMovies(data.results);
+  //   //   });
+  // };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      fetch(SEARCH_API + searchTerm)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          SetMovies(data.results);
+        });
+    }
+
+    setSearchTerm("");
+  };
+
   return (
     <div className="App">
-      <div className="header">
-        <h2>MOVIES APP</h2>
-      </div>
+      <h2>MOVIES APP</h2>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchTerm}
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+            }}
+          />
+          <FaSearch type="submit" onClick={handleOnSubmit} />
+        </form>
+      </header>
       {/* display a list of movies and pass props to Movie component  */}
       <div className="movie-container">
         {movies.length > 0 &&
